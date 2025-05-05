e73a246758af390d4cd2d738acc8478505618d1f
Yamen <healpy245@gmail.com>
2025-05-05 14:59:44 +0300

    newest 2

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -12,5 +12,60 @@ use Illuminate\Support\Facades\Auth;
 class LeadController extends Controller
 {
 
-    
+    public function index()
+    {
+        $leads = Lead::where('company_id', Auth::user()->company_id)->get();
+        return response()->json(['leads' => $leads], 200);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'phone' => 'required|string',
+            'email' => 'nullable|email',
+            'address' => 'nullable|string',
+            'user_id' => 'nullable|exists:users,id'
+        ]);
+
+        $lead = Lead::create([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'address' => $request->address,
+            'company_id' => Auth::user()->company_id,
+            'user_id' => $request->user_id,
+        ]);
+        return response()->json(['lead' => $lead], 200);
+    }
+
+    public function show($id)
+    {
+        $lead = Lead::where('company_id', Auth::user()->company_id)->findOrFail($id);
+        return response()->json(['lead' => $lead], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $lead = Lead::where('company_id', Auth::user()->company_id)->findOrFail($id);
+        $request->validate([
+            'name' => 'string',
+            'phone' => 'string',
+            'email' => 'email',
+            'address' => 'string',
+            'status' => 'string',
+            'user_id' => 'exists:users,id'
+        ]);
+
+        $lead->update($request->only([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'address' => $request->address,
+            'status' => $request->status,
+            'user_id' => $request->user_id
+
+        ]));
+
+        return response()->json(['lead edited'=> $lead], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LeadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,5 +9,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('lead',LeadController::class);
 
+Route::middleware(['auth:sanctum', 'role:company_admin,user'])->group(function () {
+    Route::apiResource('leads', LeadController::class);
+});
+
+Route::middleware(['auth:sanctum', 'role:super_admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index']);
+});
